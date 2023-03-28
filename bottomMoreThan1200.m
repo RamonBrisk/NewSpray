7de2dc1613f8af2791d3539547084bc481f57b19
Ramon <l554058765@live.com>
2023-03-28 16:49:42 +0800

    找出下半件墙面上的圆弧

--- bottomMoreThan1200.m
+++ bottomMoreThan1200.m
@@ -1,5 +1,6 @@
 function bottomMoreThan1200(clamp, radius, deltaZ)
 
+close all;
 debug = 1;
 toolDistance = 190;
 trackWidth = 190;
@@ -9,11 +10,10 @@ arcEndMark = [-150000, -150000, -150000, -150000, -150000, -150000];
 
 
 
-pcshow(clamp.pointCloud);
-
 h = clamp.panel - deltaZ; % 高度
 r = radius;
-pos = [(clamp.xmin + clamp.xmax)/2, clamp.ymin]; % 圆心位置
+pc = [(clamp.xmin + clamp.xmax)/2, clamp.ymin, h];
+pos = [pc(1), pc(2)]; % 圆心位置
 t=0:0.1:(2*pi);  % 圆滑性设置
 t=[t,0];
 matchingCircle = [(pos(1)+r*sin(t))', (pos(2)+ones(size(t)))',(h+r*cos(t))'];
@@ -46,6 +46,34 @@ endSprayTrack = [endSprayTrack(1,:);arcStartMark;endSprayTrack;arcEndMark];
 
 
 
+matchingCircleWall = matchingCircle;
+matchingCircleWall(:,1) = matchingCircleWall(:,1) - 1.1*radius - toolDistance;
+matchingCircleWall(:,3) =  matchingCircleWall(:,3) + 1.8*radius + toolDistance;
+
+pc(1) = pc(1) - 1.1*radius - toolDistance;
+
+pc(3) = pc(3) + 1.8*radius + toolDistance;
+
+[row,~] = find(matchingCircleWall(:,1)>pc(1) & matchingCircleWall(:,3)<pc(3));
+
+arcWall = matchingCircleWall(row,:);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -54,16 +82,23 @@ endSprayTrack = [endSprayTrack(1,:);arcStartMark;endSprayTrack;arcEndMark];
 
 if debug == 1
 
+pcshow(clamp.pointCloud);
 hold on
 pcshow(pointCloud(matchingCircle,"Color",'cyan'));
-end
-
 restoredHeadSprayTrack = restoreTrack(headSprayTrack);
 restoredEndSprayTrack = restoreTrack(endSprayTrack);
 hold on 
 plot3(restoredHeadSprayTrack(:,1), restoredHeadSprayTrack(:,2), restoredHeadSprayTrack(:,3),'.-blue')
 hold on 
 plot3(restoredEndSprayTrack(:,1), restoredEndSprayTrack(:,2), restoredEndSprayTrack(:,3),'.-blue')
+
+% hold on
+% pcshow(pointCloud(matchingCircleWall,"Color",'cyan'));
+
+hold on
+pcshow(pointCloud(arcWall,"Color",'cyan'));
+
+end
 
 
 
